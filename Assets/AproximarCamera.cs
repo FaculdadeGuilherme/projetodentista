a29bac5e0bc390e77acb958d10fdc6ed996a99d5
Daniel Miranda <barbosamiranda418@hotmail.com>
2019-11-04 09:34:11 -0300

    Entrega dia 8 programador

---
+++ Assets/AproximarCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AproximarCamera : MonoBehaviour
+{
+    public string Target;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject sphere = GameObject.Find(Target);
+        float position = Vector3.Distance(sphere.transform.position, transform.position);
+        
+        if(position < 1.7)
+        {
+            transform.position = new Vector3(0, 0, transform.position.z);
+            Debug.Log("Mudou");
+        }
+    }
+}
